df201ca9482da15626aefd1807e76ce1f9154411
Eris M <erismathison@gmail.com>
2025-05-20 08:10:20 +1000

    Added dialogue to buttons.

--- ImageConverter/ViewModels/MainWindowViewModel.cs
+++ ImageConverter/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,8 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using ImageConverter.Services;
 
 namespace ImageConverter.ViewModels;
 
@@ -11,14 +14,35 @@ public partial class MainWindowViewModel : ViewModelBase
     private string _outputPath = "";
 
     [RelayCommand]
-    private void ChooseInputFile()
+    private async Task ChooseInputFile()
     {
-        //TODO
+        InputPath = await OpenFile() ?? "";
     }
 
     [RelayCommand]
-    private void ChooseOutputFile()
+    private async Task ChooseOutputFile()
     {
-        //TODO
+        OutputPath = await OpenFile() ?? "";
+    }
+
+    private static async Task<string?> OpenFile()
+    {
+        try
+        {
+            var service = App.Current?.ServiceProvider?.GetService(typeof(FileService));
+            if (service == null)
+                throw new NullReferenceException("No file service!");
+        
+            if (service is not FileService fileService)
+                throw new Exception("File service not FileService!");
+        
+            var file = await fileService.OpenFile();
+            return file?.Path.AbsolutePath;
+        }
+        catch (Exception e)
+        {
+            await Console.Error.WriteLineAsync(e.ToString());
+            return null;
+        }
     }
 }
