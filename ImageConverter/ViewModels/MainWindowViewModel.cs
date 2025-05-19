a719928ddfdd41a9a65ef462d36ac63695c38bcc
Eris M <erismathison@gmail.com>
2025-05-19 21:47:43 +1000

    Added button commands and text properties.

--- ImageConverter/ViewModels/MainWindowViewModel.cs
+++ ImageConverter/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,24 @@
-﻿namespace ImageConverter.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace ImageConverter.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
+    [ObservableProperty]
+    private string _inputPath = "";
+    [ObservableProperty]
+    private string _outputPath = "";
+
+    [RelayCommand]
+    private void ChooseInputFile()
+    {
+        //TODO
+    }
+
+    [RelayCommand]
+    private void ChooseOutputFile()
+    {
+        //TODO
+    }
 }
